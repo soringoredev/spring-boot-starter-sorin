e1313ecee097ffd32304655147d1f1ef6ea73f1e
Sorin Gore <soringoredev@gmail.com>
2025-09-04 16:44:03 +0300

    Initial commit

---
+++ src/main/java/com/example/Spring/Boot/Starter/Introduction/SpringBootStarterIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.Starter.Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/Boot/Starter/Introduction/SpringBootStarterIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.Starter.Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
